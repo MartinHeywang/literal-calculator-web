7573ecf739d0b32d6f708ad3263f26d81be556be
Martin Heywang <martin.heywang1@gmail.com>
2022-03-16 10:55:22 +0100

    Moved from variation rate to literal calculator

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,25 +6,35 @@ const e = {
         function: document.querySelector<HTMLSpanElement>("#function-def")!,
         a: document.querySelector<HTMLSpanElement>("#a-def")!
     },
+    result: {
+        p: document.querySelector<HTMLParagraphElement>(".result__paragraph")!,
+    },
     compute: document.querySelector<HTMLButtonElement>("#compute")!
 }
 
 e.compute.addEventListener("click", () => {
     const textDef = e.def.function.textContent || "";
-    // const abscissa = parseFloat(e.def.a.textContent || "1");
 
-    const def = createExpression(textDef);
+    try {
+        const def = createExpression(textDef);
+        const reduced = reduce(def);
 
-    
-    const reduced = reduce(def);
+        console.groupCollapsed("%cParsed", "color: orange; font-size: 1.2rem");
+        console.log(def)
+        console.log(stringifyExpression(def))
+        console.groupEnd();
+        
+        console.group("%cReduced", "color: yellow; font-size: 1.2rem")
+        console.log(reduced);
+        console.log(stringifyExpression(reduced));
+        console.groupEnd();
 
-    console.groupCollapsed("%cParsed", "color: orange; font-size: 1.2rem");
-    console.log(def)
-    console.log(stringifyExpression(def))
-    console.groupEnd();
-    
-    console.group("%cReduced", "color: yellow; font-size: 1.2rem")
-    console.log(reduced);
-    console.log(stringifyExpression(reduced));
-    console.groupEnd();
+        e.result.p.textContent = `${textDef} = ${stringifyExpression(reduced)}`
+    } catch (err) {
+        e.result.p.textContent = "An error occurred. Double-check the input literal expression.";
+
+        console.log(err);
+    }
+
+
 })
