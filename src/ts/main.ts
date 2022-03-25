b8787f590cc0a2ffb494b18078356189b204a3b7
Martin Heywang <martin.heywang1@gmail.com>
2022-03-25 17:54:51 +0100

    Added issue link

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,36 +4,56 @@ import { createExpression, reduce, stringifyExpression } from "./expression";
 const e = {
     def: {
         function: document.querySelector<HTMLSpanElement>("#function-def")!,
-        a: document.querySelector<HTMLSpanElement>("#a-def")!
+        a: document.querySelector<HTMLSpanElement>("#a-def")!,
     },
     result: {
         p: document.querySelector<HTMLParagraphElement>(".result__paragraph")!,
+        issue: document.querySelector<HTMLAnchorElement>(".result__issue")!,
     },
-    compute: document.querySelector<HTMLButtonElement>("#compute")!
-}
+    compute: document.querySelector<HTMLButtonElement>("#compute")!,
+};
 
 e.compute.addEventListener("click", () => {
     const textDef = e.def.function.textContent || "";
+
+    const summaryStr = (str: string, length = 35) =>
+        str.length <= length ? str : str.slice(0, length) + "...";
 
     try {
         const def = createExpression(textDef);
         const reduced = reduce(def);
 
         console.groupCollapsed("%cParsed", "color: orange; font-size: 1.2rem");
-        console.log(def)
-        console.log(stringifyExpression(def))
+        console.log(def);
+        console.log(stringifyExpression(def));
         console.groupEnd();
-        
-        console.group("%cReduced", "color: yellow; font-size: 1.2rem")
+
+        console.group("%cReduced", "color: yellow; font-size: 1.2rem");
         console.log(reduced);
         console.log(stringifyExpression(reduced));
         console.groupEnd();
 
-        e.result.p.textContent = `${textDef} = ${stringifyExpression(reduced)}`
-    } catch (err) {
-        e.result.p.textContent = "An error occurred. Double-check the input literal expression.";
+        const issueTitle = `Unexpected result when reducing: '${summaryStr(textDef)}'`;
+        const issueBody = `${issueTitle}\n\nInput: '${textDef}'\nCurrent output: ${stringifyExpression(
+            reduced
+        )}\nExpected output: please complete here`;
+
+        e.result.p.textContent = `${textDef} = ${stringifyExpression(reduced)}`;
+        e.result.issue.href = `https://github.com/martinheywang/literal-calculator-web/issues/new?title=${encodeURIComponent(
+            issueTitle
+        )}&body=${encodeURIComponent(issueBody)}`;
+    } catch (err: any) {
+        const message: string = err.message;
+        const issueTitle = `Error when reducing: '${summaryStr(textDef)}'`;
+        const issueBody = `${issueTitle}\n\nInput: '${textDef}'\nMessage: "${message}"`;
+
+        e.result.p.textContent = `An error occurred. "${message}"`;
+        e.result.issue.href = `https://github.com/martinheywang/literal-calculator-web/issues/new?title=${encodeURIComponent(
+            issueTitle
+        )}&body=${encodeURIComponent(issueBody)}`;
 
         console.log(err);
     }
 
-})
+    e.result.issue.style.display = "unset";
+});
